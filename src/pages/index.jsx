4b3dcb3adfaf80aeda0ed1b7d673c8bed76a2ef5
Alber Erre <arblugo@gmail.com>
2021-08-13 14:05:38 +0100

    feat: update header title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,17 +5,18 @@ const IndexPage = () => {
   const { popularGenres: genres } = useGenres();
 
   const numberOfshows = genres.reduce((acc, genre) => acc + genre.numShows, 0);
+  const title = `Shows by Genre (${numberOfshows})`;
 
   // TODO: navigate to an error page on this condition
   if (!genres.length) return null;
 
   return (
-    <Layout title="Shows by Genre">
+    <Layout title={title}>
       <section>
-        <h1>{`Shows by Genre (${numberOfshows})`}</h1>
+        <h1>{title}</h1>
         <ul>
-          {genres.map(({ id, title }) => (
-            <li key={id}>{title}</li>
+          {genres.map(({ id, title: genreTitle }) => (
+            <li key={id}>{genreTitle}</li>
           ))}
         </ul>
       </section>
