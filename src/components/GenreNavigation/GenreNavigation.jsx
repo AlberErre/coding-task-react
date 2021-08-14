cd8695176c62c7a772e33c66372a7b85053d134d
Alber Erre <arblugo@gmail.com>
2021-08-14 14:44:31 +0100

    style: update navigation styling

--- src/components/GenreNavigation/GenreNavigation.jsx
+++ src/components/GenreNavigation/GenreNavigation.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import styles from './GenreNavigation.module.css';
 
 export const GenreNavigation = () => {
+  const router = useRouter();
+
+  const isCurrentRoute = (path) => router.pathname === path;
+
   return (
-    <nav className={styles.navigation}>
-      <ul>
-        <li>
+    <nav>
+      <ul className={styles.navigation}>
+        <li
+          className={styles.link}
+          style={isCurrentRoute('/') ? { textDecorationLine: 'underline' } : {}}
+        >
           <Link href="/">
             <a>Shows</a>
           </Link>
         </li>
-        <li>
+        <li
+          className={styles.link}
+          style={
+            isCurrentRoute('/favourites')
+              ? { textDecorationLine: 'underline' }
+              : {}
+          }
+        >
           <Link href="/favourites">
             <a>Favourites</a>
           </Link>
