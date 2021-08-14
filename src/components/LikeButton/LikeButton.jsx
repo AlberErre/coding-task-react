58cf9845f2778db3709125ca62d3cb21ca10df0f
Alber Erre <arblugo@gmail.com>
2021-08-14 15:01:46 +0100

    fix: solve accessibility issue using aria-label

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -5,14 +5,11 @@ import styles from './LikeButton.module.css';
 
 export const LikeButton = ({ isLiked, onClick }) => (
   <button
+    aria-label="Like button"
     className={classnames('buttonReset', styles.likeButton)}
     onClick={onClick}
     type="button"
   >
-    {/* //NOTE: add invisible text to handle accessibility
-    source -> https://dequeuniversity.com/rules/axe/3.5/button-name?application=axeAPI */}
-    <p className="invisibleText">like button</p>
-
     <svg height="26" width="28" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M19.5 0c-2.889 0-5.489 1.869-6.5 4.313C11.989 1.869 9.389 0 6.5 0 2.889 0 0 2.731 0 6.469 0 11.5 5.489 15.525 13 23c7.511-7.475 13-11.5 13-16.531C26 2.73 23.111 0 19.5 0z"
